b7fd6acd9e9cfc6eefd2c21111af19b81f9d4431
Phoebe Stanley <phoebewangustanley@gmail.com>
2025-08-31 00:53:18 +0300

    Added Header, MainContent, and Footer components and updated App.jsx to include them

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,15 +1,19 @@
-import WelcomeMessage from './components/WelcomeMessage';
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
 
 function App() {
   return (
     <div>
-      {/* Using our custom component */}
-      <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
     </div>
   );
 }
 
 export default App;
+
 
 /*import { useState } from 'react'
 import reactLogo from './assets/react.svg'
