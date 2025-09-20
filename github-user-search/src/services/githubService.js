9064a44f59e58d67fc23676ef4d595dd3a42e199
phoebestanley <phoebewangustanley@gmail.com>
2025-09-20 15:55:43 +0300

    Update githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -2,7 +2,20 @@ import axios from "axios";
 
 const BASE_URL = "https://api.github.com";
 
-export const fetchUserData = async (username) => {
-  const response = await axios.get(`${BASE_URL}/users/${username}`);
-  return response.data;
+// Advanced search with multiple parameters
+export const searchUsers = async (username, location, minRepos) => {
+  try {
+    let query = "";
+
+    if (username) query += `${username}+`;
+    if (location) query += `location:${location}+`;
+    if (minRepos) query += `repos:>=${minRepos}+`;
+
+    query = query.trim().replace(/\+$/, ""); // remove trailing +
+
+    const response = await axios.get(`${BASE_URL}/search/users?q=${query}`);
+    return response.data.items;
+  } catch (error) {
+    throw new Error("Failed to fetch users");
+  }
 };
