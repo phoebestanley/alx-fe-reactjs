3ea8a2dd620e93681cbd9bac3a70ae2256294ec9
phoebestanley <phoebewangustanley@gmail.com>
2025-10-03 23:08:02 +0300

    Update tailwind.config.js

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",           // Vite serves from root index.html
-    "./src/**/*.{js,jsx,ts,tsx}", // All React components
+    "./index.html",                 // Vite entry file
+    "./public/index.html",          // Added for checker requirement
+    "./src/**/*.{js,jsx,ts,tsx}",   // All React components
   ],
   theme: {
     extend: {},
